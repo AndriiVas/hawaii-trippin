55fe44a80060d160efe218971169bd6dd04c39fb
Andrii Vasylchenko <andrvas92@gmail.com>
2021-05-15 16:45:40 -0400

    updated title

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -16,7 +16,7 @@ function Cards() {
               path="/maui"
             />
             <CardItem
-              src="images/kauai-logo1.png"
+              src="images/kauai-logo1.jpg"
               text="The oldest in Hawaii’s modern-day chain, Kauaʻi has a reputation for being a lush island; hence, its nickname as the Garden Island"
               label="Greenest"
               path="/kauai"
@@ -24,14 +24,14 @@ function Cards() {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="images/kona-logo1.png"
+              src="images/kona-logo1.jpg"
               text="Kona Coast is the prime area on Hawaii Islands for discovering marine life"
               label="Biggest"
               path="/kona"
             />
 
             <CardItem
-              src="images/oahu-logo1.png"
+              src="images/oahu-logo1.jpg"
               text="When you think of Oahu you think of the three S’s — sun, surf, and shopping"
               label="Touristy"
               path="/oahu"
